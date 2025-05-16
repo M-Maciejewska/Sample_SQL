e7cd704eb0ee8d00eab9442e2d36fe2359568fb9
M-Maciejewska <mm.maciejewska99@gmail.com>
2025-05-16 22:40:48 +0200

    Update Task4.sql

--- Task4.sql
+++ Task4.sql
@@ -1,4 +1,6 @@
-/* Write a query that changes all missing values ( NULL ) to 'WAITING', then craft a second query to return all columns of all rows that have the new status. Order them ascending, by the column id */
+/* Write a query that changes all missing values ( NULL ) 
+to 'WAITING', then craft a second query to return all columns 
+of all rows that have the new status. Order them ascending, by the column id */
 
 UPDATE 
 	projects 
@@ -8,7 +10,8 @@ WHERE
 	status IS NULL;
 
 SELECT * FROM 
-		projects 
-	WHERE 
-		status = 'WAITING' 
-ORDER BY id ASC
+	projects 
+WHERE 
+	status = 'WAITING' 
+ORDER BY 
+	id ASC;
