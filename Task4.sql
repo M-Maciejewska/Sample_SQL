ee3aba414bcd4fc828c3f92447ad63b6c1f9def6
M-Maciejewska <mm.maciejewska99@gmail.com>
2025-05-16 22:39:00 +0200

    Add files via upload

---
+++ Task4.sql
@@ -0,0 +1,14 @@
+/* Write a query that changes all missing values ( NULL ) to 'WAITING', then craft a second query to return all columns of all rows that have the new status. Order them ascending, by the column id */
+
+UPDATE 
+	projects 
+SET 
+	status = 'WAITING' 
+WHERE 
+	status IS NULL;
+
+SELECT * FROM 
+		projects 
+	WHERE 
+		status = 'WAITING' 
+ORDER BY id ASC
