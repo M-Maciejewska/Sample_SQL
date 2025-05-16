c36c22f84a1f180742d9ef71b1fc3b178843950e
M-Maciejewska <mm.maciejewska99@gmail.com>
2025-05-16 22:41:55 +0200

    Update WHERE_CLAUSE.sql

--- WHERE_CLAUSE.sql
+++ WHERE_CLAUSE.sql
@@ -1,9 +1,12 @@
 SELECT 
 	shipping_mode,
-	count(order_id),				AS order_count		
-	count(DISTINCT customer_id)			AS nr_of_unique_customer
-FROM orders
+	count(order_id),			AS order_count		
+	count(DISTINCT customer_id)		AS nr_of_unique_customer
+FROM 
+	orders
 WHERE 
 	order_id <> 384
-GROUP BY 1
-ORDER BY shipping_mode DESC;
+GROUP BY 
+	1
+ORDER BY 
+	shipping_mode DESC;
